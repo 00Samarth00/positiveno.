b0ec597fcdb475e58a9a6cd8dce9b3ba01264f2b
00Samarth00 <71335163+00Samarth00@users.noreply.github.com>
2020-09-17 21:23:48 +0530

    w/o functions

--- positive no.py
+++ positive no.py
@@ -1,18 +1,20 @@
 #THIS PROGRAM FINDS THE POSITIVE NOS. FROM THE LIST OF 5 NUMBERS.
 
-def pos_(n1):
-    return n1 if n1>=0 else ''
-    
-
 l1=[]
+out1=[]
 while(True):
     for i in range(0,5):
         x=eval(input('ENTER VALUE {} IN LIST:'.format(i+1)))
         l1.append(x)
     print('GIVEN NUMBERS:-',l1)
     print('THE POSITIVE NUMBERS OUT OF THESE ARE:')
+
     for q in range(0,5):
-        print(pos_(l1[q]))
+        if(l1[q]>=0):
+            out1.append(l1[q])
+        else:
+            None
+    print(out1)
 
 
     con_=input('DO YOU WANT TO USE ANOTHER LIST OF NUMBERS?(Y/N)').casefold()
@@ -21,6 +23,7 @@ while(True):
         continue
     elif(con_=='no' or con_=='n'):
         break
+
 
 
 
